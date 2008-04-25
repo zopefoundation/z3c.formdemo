658057d8cfcfdc48fa2b7d71c4d8d4829ce448d0
Stephan Richter <stephan.richter@gmail.com>
2008-04-25 01:33:06 +0000

    version bump and a few changes for the next release.

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ def read(*rnames):
 
 setup (
     name='z3c.formdemo',
-    version='1.5.2',
+    version='1.5.3dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
-    author_email = "zope3-dev@zope.org",
+    author_email = "zope-dev@zope.org",
     description = "A set of demo applications for z3c.form and z3c.formui",
     long_description=(
         read('README.txt')
@@ -44,7 +44,7 @@ setup (
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Framework :: Zope3'],
-    url = 'http://cheeseshop.python.org/pypi/z3c.formdemo',
+    url = 'http://pypi.python.org/pypi/z3c.formdemo',
     packages = find_packages('src'),
     include_package_data = True,
     package_dir = {'':'src'},
